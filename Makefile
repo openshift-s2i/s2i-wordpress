eee86ba97cda81818337bb361c87525a9708976a
Magnus Bengtsson <magnus.bengtsson@expressen.se>
2018-09-07 22:21:57 +0200

    Bump to 4.9.8

--- Makefile
+++ Makefile
@@ -2,8 +2,7 @@ BASE_IMAGE_NAME=s2i-wordpress
 NAMESPACE=bonniernews
 # https://wordpress.org/download/release-archive/
 VERSIONS = \
-4.7.9:dbcc2b76d10c8c05d50ef6be2698d15eb3d76dcc \
-4.9.4:0e630bf940fd586b10e099cd9195b3e825fb194c
+4.9.8:0945bab959cba127531dceb2c4fed81770812b4f
 
 include build/common.mk
 
@@ -11,3 +10,6 @@ include build/common.mk
 #4.5.8:cf4b0a36f82f942696abb17cb48719e4fa256722 \
 #4.6.5:b73d8a86bbea77e5045157ac7a7d65502cd910bc \
 #4.7.4:153592ccbb838cafa1220de9174ec965df2e9e1a \
+#4.7.9:dbcc2b76d10c8c05d50ef6be2698d15eb3d76dcc \
+#4.9.4:0e630bf940fd586b10e099cd9195b3e825fb194c \
+# 4.9.6:40616b40d120c97205e5852c03096115c2fca537
--- build/build.sh
+++ build/build.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 set -e -o pipefail
+docker pull centos/php-70-centos7:latest
 ## php 5.6
-for version in ${VERSIONS}; do
-    _version=$(echo ${version} | cut -d ":" -f1)
-    _short_version=$(echo ${_version//./} | cut -c 1,2 )
-    _sha1=$(echo ${version} | cut -d ":" -f2)
-    echo "=== Building Wordpress s2i php 5.6 v${_version}"
-    docker build -f Dockerfile.5.6 --build-arg WORDPRESS_VERSION=${_version} --build-arg WORDPRESS_SHA1=${_sha1} \
-    -t ${NAMESPACE}/${BASE_IMAGE_NAME}${_short_version}:${_version} \
-    --label io.bonniernews.wordpress.version="${_version}" \
-    --label io.bonniernews.wordpress.build.date="$(date +%c)" \
-    --label io.bonniernews.wordpress.build.user="${USER}" .
-done
+#for version in ${VERSIONS}; do
+#    _version=$(echo ${version} | cut -d ":" -f1)
+#    _short_version=$(echo ${_version//./} | cut -c 1,2 )
+#    _sha1=$(echo ${version} | cut -d ":" -f2)
+#    echo "=== Building Wordpress s2i php 5.6 v${_version}"
+#    docker build -f Dockerfile.5.6 --build-arg WORDPRESS_VERSION=${_version} --build-arg WORDPRESS_SHA1=${_sha1} \
+#    -t ${NAMESPACE}/${BASE_IMAGE_NAME}${_short_version}:${_version} \
+#    --label io.bonniernews.wordpress.version="${_version}" \
+#    --label io.bonniernews.wordpress.build.date="$(date +%c)" \
+#    --label io.bonniernews.wordpress.build.user="${USER}" .
+#done
 
 ## php 7.0
 for version in ${VERSIONS}; do
@@ -19,7 +20,7 @@ for version in ${VERSIONS}; do
     _short_version=$(echo ${_version//./} | cut -c 1,2 )
     _sha1=$(echo ${version} | cut -d ":" -f2)
     echo "=== Building Wordpress s2i php 7.0 v${_version}"
-    docker build -f Dockerfile.7.0 --build-arg WORDPRESS_VERSION=${_version} --build-arg WORDPRESS_SHA1=${_sha1} \
+    docker build --rm -f Dockerfile.7.0 --build-arg WORDPRESS_VERSION=${_version} --build-arg WORDPRESS_SHA1=${_sha1} \
     -t ${NAMESPACE}/${BASE_IMAGE_NAME}-php70-${_short_version}:${_version} \
     --label io.bonniernews.wordpress.version="${_version}" \
     --label io.bonniernews.wordpress.build.date="$(date +%c)" \
